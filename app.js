afebd62aa1089fee7931eeaccc7fc394785c3ea9
Dorian Morgan <dmorgan@pickyourplum.com>
2017-08-08 00:02:32 -0600

    basic logic added

--- app.js
+++ app.js
@@ -34,5 +34,44 @@
 // I am going to first just Wrap this in a function called depOrderer
 var depOrderer = function(array){
 
+  var packages = [];
+  // the final list in order
+  var result = [];
+
+  for (var i = 0; i < array.length; i ++) {
+    //need to slipt the array upon iteration
+  var arraySplit = array[i].split(':');
+
+    var dependency = arraySplit[0];
+
+    var value = arraySplit[1].trim();
+    console.log(dependency)
+    packages[dependency] = value;
+
+
+  }
+
+// Search down packages dependencies
+  for(dependency in packages) {
+   if (packages.hasOwnProperty(dependency)) {
+      seen(dependency);
+
+    }
+  }
+
+  function seen(dependency) {
+   if (!packages.hasOwnProperty(dependency)) {
+     return false;
+    }
+
+   if (packages[dependency] !== '') {
+      seen(packages[dependency]);
+   }
+    result.push(dependency);
+    // delete unwanted dependency
+    delete packages[dependency];
+  }
+
+  return(result);
 
 };
