7cc749ed2dd988bc740b13e611dd0a00844069ca
Dorian Morgan <dmorgan@pickyourplum.com>
2017-08-07 22:20:49 -0600

    understand the test data and what function should do

---
+++ app.js
@@ -0,0 +1,38 @@
+// Test Data and Solutions
+//
+// [ "KittenService: CamelCaser", "CamelCaser: " ]
+// Should return "CamelCaser, KittenService"
+//
+// ["KittenService: ",
+//  "Leetmeme: Cyberportal",
+//  "Cyberportal: Ice",
+//  "CamelCaser: KittenService",
+//  "Fraudstream: Leetmeme",
+//  "Ice: "
+// ];
+// Should return "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream"
+//
+// [
+//   "1: ",
+//   "4: 3",
+//   "3: 2",
+//   "5: 1",
+//   "6: 4",
+//   "2: "
+// ];
+// Should return '1, 2, 3, 4, 5, 6'
+//
+// ["KittenService: ",
+// "Leetmeme: Cyberportal",
+// "Cyberportal: Ice",
+// "CamelCaser: KittenService",
+// "Fraudstream: ",
+// "Ice: Leetmeme"]
+// Should Reject
+
+
+// I am going to first just Wrap this in a function called depOrderer
+var depOrderer = function(array){
+
+
+};
